eb58111d4704d47257efaeffae1516a8bd1dc79d
mercaderojsh-source <mercaderojsh@gmail.com>
2026-07-20 06:38:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from exchange.bitget import get_candles
 from indicators.ema import calculate_ema
 from indicators.atr import calculate_atr
 from indicators.rsi import calculate_rsi
+from strategy.signal_engine import generate_signal
 
 print("=" * 40)
-print("🚀 MARKET ANALYZER")
+print("🚀 SIGNAL ENGINE")
 print("=" * 40)
 
 candles = get_candles("BTCUSDT")
@@ -20,28 +21,28 @@ if candles["code"] == "00000":
     ema50 = calculate_ema(closes, 50)
 
     atr = calculate_atr(highs, lows, closes)
-
     rsi = calculate_rsi(closes)
 
-    print("EMA 9 :", round(ema9, 2))
-    print("EMA 21:", round(ema21, 2))
-    print("EMA 50:", round(ema50, 2))
-    print("ATR   :", round(atr, 2))
-    print("RSI   :", rsi)
-
-    if ema9 > ema21 > ema50:
-        trend = "BULLISH"
-    elif ema9 < ema21 < ema50:
-        trend = "BEARISH"
-    else:
-        trend = "SIDEWAYS"
+    score, reasons = generate_signal(
+        ema9,
+        ema21,
+        ema50,
+        atr,
+        rsi
+    )
 
     print()
-    print("Trend:", trend)
+    print("Signal Score:", score, "/3")
+    print()
 
-    if rsi > 70:
-        print("RSI Status: OVERBOUGHT")
-    elif rsi < 30:
-        print("RSI Status: OVERSOLD")
+    for reason in reasons:
+        print(reason)
+
+    print()
+
+    if score == 3:
+        print("🚀 STRONG TRADE SETUP")
+    elif score == 2:
+        print("👀 WATCHLIST")
     else:
-        print("RSI Status: NORMAL")
+        print("⛔ NO TRADE")
