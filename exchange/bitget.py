3e1d9bb27025fb958b28cda13af780e2629ffee4
mercaderojsh-source <mercaderojsh@gmail.com>
2026-07-20 06:13:00 +0800

    Update bitget.py

--- exchange/bitget.py
+++ exchange/bitget.py
@@ -9,3 +9,18 @@ def get_ticker(symbol):
     response = requests.get(url)
 
     return response.json()
+
+
+def get_candles(symbol, granularity="1m", limit="100"):
+    url = f"{BASE_URL}/api/v2/mix/market/candles"
+
+    params = {
+        "symbol": symbol,
+        "granularity": granularity,
+        "limit": limit,
+        "productType": "USDT-FUTURES"
+    }
+
+    response = requests.get(url, params=params)
+
+    return response.json()
