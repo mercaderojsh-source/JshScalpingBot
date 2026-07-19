83491f819ea14520e47694135a9948b00290873c
mercaderojsh-source <mercaderojsh@gmail.com>
2026-07-20 05:03:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from exchange.bitget import get_ticker
+
+PAIRS = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "SOLUSDT",
+    "XRPUSDT",
+    "BGBUSDT"
+]
+
+
 def main():
     print("=" * 40)
     print("🚀 JshScalpingBot Started")
-    print("Version: 0.1.0")
     print("=" * 40)
+
+    for pair in PAIRS:
+        data = get_ticker(pair)
+        print(pair)
+        print(data)
+        print("-" * 40)
 
 
 if __name__ == "__main__":
