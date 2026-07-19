1596200be000d908eeb3d9b4ae07793beab4e52f
mercaderojsh-source <mercaderojsh@gmail.com>
2026-07-20 05:43:06 +0800

    Create telegram/telegram_bot.py

---
+++ telegram/telegram_bot.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def send_message(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        json={
+            "chat_id": CHAT_ID,
+            "text": message
+        }
+    )
