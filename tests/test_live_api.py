9578c2238dc544a795195dc3eef358ae6ed25428
mercaderojsh-source <mercaderojsh@gmail.com>
2026-07-21 06:30:17 +0800

    Update test_live_api.py

--- tests/test_live_api.py
+++ tests/test_live_api.py
@@ -1,5 +1,9 @@
 from trading.live_trader import LiveTrader
-from exchange.bitget import get_symbol_rules
+from exchange.bitget import (
+    get_symbol_rules,
+    set_leverage
+)
+from config import LEVERAGE
 
 
 def main():
@@ -14,6 +18,10 @@ def main():
     print("\n===== BTC RULES =====")
     rules = get_symbol_rules("BTCUSDT")
     print(rules)
+
+    print("\n===== SET LEVERAGE =====")
+    response = set_leverage("BTCUSDT", LEVERAGE)
+    print(response)
 
 
 if __name__ == "__main__":
