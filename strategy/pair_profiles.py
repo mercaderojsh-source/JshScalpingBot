c32918f05770ed76fb5560e2a9ae6dfc737aa1b6
mercaderojsh-source <mercaderojsh@gmail.com>
2026-07-21 04:59:03 +0800

    Create strategy/pair_profiles.py

---
+++ strategy/pair_profiles.py
@@ -0,0 +1,43 @@
+PAIR_PROFILES = {
+
+    "BTCUSDT": {
+        "min_score": 70,
+        "min_atr": 0.05,
+        "min_rsi": 28,
+        "max_rsi": 72,
+        "require_htf": False,
+    },
+
+    "ETHUSDT": {
+        "min_score": 70,
+        "min_atr": 0.05,
+        "min_rsi": 28,
+        "max_rsi": 72,
+        "require_htf": False,
+    },
+
+    "SOLUSDT": {
+        "min_score": 70,
+        "min_atr": 0.05,
+        "min_rsi": 28,
+        "max_rsi": 72,
+        "require_htf": False,
+    },
+
+    "XRPUSDT": {
+        "min_score": 70,
+        "min_atr": 0.05,
+        "min_rsi": 28,
+        "max_rsi": 72,
+        "require_htf": False,
+    },
+
+    "BGBUSDT": {
+        "min_score": 70,
+        "min_atr": 0.05,
+        "min_rsi": 28,
+        "max_rsi": 72,
+        "require_htf": False,
+    }
+
+}
